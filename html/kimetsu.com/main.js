8124a18e160398a8c483a8a3fc526643830c162a
thangphan37 <pcthangk59@gmail.com>
2021-05-15 15:27:57 +0900

    add scroll effect

--- html/kimetsu.com/main.js
+++ html/kimetsu.com/main.js
@@ -5,7 +5,7 @@ const MARGIN_DISTANCE = 48
 const SLIDE_SIZE = 742
 const ASIDE_SIZE = (slides.offsetWidth - SLIDE_SIZE - MARGIN_DISTANCE * 2) / 2
 const INITIAL_SCROLL_LEFT = SLIDE_SIZE - ASIDE_SIZE + MARGIN_DISTANCE / 2
-const SWIPE_SIZE = SLIDE_SIZE - ASIDE_SIZE - 50
+const SWIPE_SIZE = SLIDE_SIZE - ASIDE_SIZE - 250
 
 let currentIndex = 0
 let start
@@ -20,23 +20,51 @@ slideButtons.forEach((btn) =>
 )
 
 slides.addEventListener('mousemove', function (e) {
-  if (e.x > start) {
-    console.log('e.x, start', e.x, start)
-    slides.scrollLeft += e.x - start
+  if (e.x <= 100 || e.x >= 1565) {
+    return
+  }
+
+  if (e.x < start && slides.scrollLeft <= 5875) {
+    slides.scrollLeft += (start - e.x) * 200
+  } else if (e.x > start) {
+    slides.scrollLeft -= (e.x - start) * 200
   }
 })
 
 slides.addEventListener('mousedown', function (e) {
-  console.log('e', e)
   start = e.x
 })
 
 slides.addEventListener('mouseup', function (e) {
-  end = e.x
   start = undefined
 })
 
 slides.addEventListener('mouseup', function (e) {
+  if (slides.scrollLeft >= 4295) {
+    if (currentIndex === 3) {
+      currentIndex++
+    } else {
+      currentIndex = 1
+    }
+    slides.scrollLeft =
+      (SLIDE_SIZE + MARGIN_DISTANCE) * currentIndex + INITIAL_SCROLL_LEFT
+    return
+  }
+
+  if (e.x <= 0) {
+    currentIndex++
+    slides.scrollLeft =
+      (SLIDE_SIZE + MARGIN_DISTANCE) * currentIndex + INITIAL_SCROLL_LEFT
+    return
+  }
+
+  if (e.x >= 1565) {
+    currentIndex--
+    slides.scrollLeft =
+      (SLIDE_SIZE + MARGIN_DISTANCE) * currentIndex + INITIAL_SCROLL_LEFT
+    return
+  }
+
   const previousScrollLeft =
     (SLIDE_SIZE + MARGIN_DISTANCE) * currentIndex + INITIAL_SCROLL_LEFT
 
