bb3d6ec8c959480a556b089e60b425f1cc13223f
thangphan37 <pcthangk59@gmail.com>
2021-05-16 15:20:07 +0900

    edit scroll

--- html/kimetsu.com/main.js
+++ html/kimetsu.com/main.js
@@ -6,28 +6,42 @@ const SLIDE_SIZE = 742
 const ASIDE_SIZE = (slides.offsetWidth - SLIDE_SIZE - MARGIN_DISTANCE * 2) / 2
 const INITIAL_SCROLL_LEFT = SLIDE_SIZE - ASIDE_SIZE + MARGIN_DISTANCE / 2
 const SWIPE_SIZE = SLIDE_SIZE - ASIDE_SIZE - 250
+const FIRST_SCROLL_LEFT =
+  (SLIDE_SIZE + MARGIN_DISTANCE) * 1 + INITIAL_SCROLL_LEFT
+const LAST_SCROLL_LEFT =
+  (SLIDE_SIZE + MARGIN_DISTANCE) * 5 + INITIAL_SCROLL_LEFT
+const NEXT_LAST_SCROLL_LEFT =
+  (SLIDE_SIZE + MARGIN_DISTANCE) * 7 + INITIAL_SCROLL_LEFT
+const SCROLL_BAR_SIZE = 200
 
 let currentIndex = 0
 let start
-let end = 0
 
-slideButtons.forEach((btn) =>
+window.onload = () => {
+  slides.scrollLeft = FIRST_SCROLL_LEFT
+}
+
+slideButtons.forEach((btn) => {
   btn.addEventListener('click', function () {
     currentIndex = parseInt(this.dataset.value)
+
+    slideButtons.forEach((btn) => (btn.style.background = '#000'))
+    this.style.background = 'orange'
+
     slides.scrollLeft =
       (SLIDE_SIZE + MARGIN_DISTANCE) * currentIndex + INITIAL_SCROLL_LEFT
-  }),
-)
+  })
+})
 
 slides.addEventListener('mousemove', function (e) {
-  if (e.x <= 100 || e.x >= 1565) {
-    return
-  }
-
-  if (e.x < start && slides.scrollLeft <= 5875) {
-    slides.scrollLeft += (start - e.x) * 200
+  // prevent scrolll to last slide faked
+  if (
+    e.x < start &&
+    slides.scrollLeft + (start - e.x) * SCROLL_BAR_SIZE <= NEXT_LAST_SCROLL_LEFT
+  ) {
+    slides.scrollLeft += (start - e.x) * SCROLL_BAR_SIZE
   } else if (e.x > start) {
-    slides.scrollLeft -= (e.x - start) * 200
+    slides.scrollLeft -= (e.x - start) * SCROLL_BAR_SIZE
   }
 })
 
@@ -39,27 +53,24 @@ slides.addEventListener('mouseup', function (e) {
   start = undefined
 })
 
+slides.addEventListener('scroll', function (e) {})
+
 slides.addEventListener('mouseup', function (e) {
-  if (slides.scrollLeft >= 4295) {
-    if (currentIndex === 3) {
-      currentIndex++
-    } else {
-      currentIndex = 1
-    }
+  // last slide and swipe
+  if (slides.scrollLeft >= LAST_SCROLL_LEFT) {
+    if (currentIndex === 5) currentIndex = 1
+    else currentIndex++
+
     slides.scrollLeft =
       (SLIDE_SIZE + MARGIN_DISTANCE) * currentIndex + INITIAL_SCROLL_LEFT
     return
   }
 
-  if (e.x <= 0) {
-    currentIndex++
-    slides.scrollLeft =
-      (SLIDE_SIZE + MARGIN_DISTANCE) * currentIndex + INITIAL_SCROLL_LEFT
-    return
-  }
+  // first slide and swipe
+  if (slides.scrollLeft < FIRST_SCROLL_LEFT) {
+    if (currentIndex === 1) currentIndex = 5
+    else currentIndex--
 
-  if (e.x >= 1565) {
-    currentIndex--
     slides.scrollLeft =
       (SLIDE_SIZE + MARGIN_DISTANCE) * currentIndex + INITIAL_SCROLL_LEFT
     return
@@ -70,19 +81,13 @@ slides.addEventListener('mouseup', function (e) {
 
   // slide right
   if (slides.scrollLeft >= previousScrollLeft + SWIPE_SIZE) {
-    if (currentIndex === 5) {
-      currentIndex = 1
-    } else {
-      currentIndex++
-    }
+    if (currentIndex === 5) currentIndex = 1
+    else currentIndex++
   }
   // slide left
   else if (slides.scrollLeft <= previousScrollLeft - SWIPE_SIZE) {
-    if (currentIndex === 1) {
-      currentIndex = 5
-    } else {
-      currentIndex--
-    }
+    if (currentIndex === 1) currentIndex = 5
+    else currentIndex--
   }
 
   slides.scrollLeft =
